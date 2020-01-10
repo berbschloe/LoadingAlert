d43b8c4cebcb4d9c2a8989b83c80e5383eab4df2
Brandon Erbschloe <berbschloe@gmail.com>
2020-01-10 10:41:27 -0600

    Initial Commit

---
+++ LoadingAlert/LoadingAlert.h
@@ -0,0 +1,19 @@
+//
+//  LoadingAlert.h
+//  LoadingAlert
+//
+//  Created by Brandon Erbschloe on 1/10/20.
+//  Copyright © 2020 Brandon Erbschloe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingAlert.
+FOUNDATION_EXPORT double LoadingAlertVersionNumber;
+
+//! Project version string for LoadingAlert.
+FOUNDATION_EXPORT const unsigned char LoadingAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingAlert/PublicHeader.h>
+
+
