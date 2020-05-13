7063fc21717fec25d873d2c2a6325a86a31671b8
Brandon Erbschloe <berbschloe@gmail.com>
2020-05-12 22:05:37 -0500

    Project refactor and made the presentation animation public.

--- LoadingAlert/LoadingAlert.swift
+++ LoadingAlert/LoadingAlert.swift
@@ -23,56 +23,72 @@
 
 import UIKit
 
-private let kCornerRadius: CGFloat = 13
-private let kSpan: CGFloat = 100
-
 public class LoadingAlertController: UIViewController {
     
-    public static var defaultActivityIndicatorColor: UIColor = .init(red: 0.58, green: 0.58, blue: 0.59, alpha: 1)
-    public static var defaultBackgroundStyle: UIBlurEffect.Style = .extraLight
+    public static var defaultActivityIndicatorColor: UIColor? = nil
+    public static var defaultBackgroundStyle: UIBlurEffect.Style = .regular
+        
+    public static var defaultSize = CGSize(width: 100, height: 100)
+    public static var defaultCornerRadius: CGFloat = 13
     
     private let backgroundView: UIVisualEffectView
     
-    private let activityIndicatorView = UIActivityIndicatorView(style: .whiteLarge)
-    private let transition = LoadingAlertTransition()
+    private let activityIndicatorView = UIActivityIndicatorView(
+        style: { if #available(iOS 13.0, *) { return .large } else { return .whiteLarge } }()
+    )
     
-    public init(activityIndicatorColor: UIColor? = nil, backgroundStyle: UIBlurEffect.Style? = nil) {
+    private let transition = AlertTransition()
+    private let size: CGSize = LoadingAlertController.defaultSize
+    private let cornerRadius: CGFloat = LoadingAlertController.defaultCornerRadius
+    
+    public init(
+        activityIndicatorColor: UIColor? = nil,
+        backgroundStyle: UIBlurEffect.Style? = nil
+    ) {
         
-        self.backgroundView = UIVisualEffectView(effect: UIBlurEffect(
-            style: backgroundStyle ?? LoadingAlertController.defaultBackgroundStyle
-        ))
-        
+        self.backgroundView = UIVisualEffectView(
+            effect: UIBlurEffect(
+                style: backgroundStyle ?? LoadingAlertController.defaultBackgroundStyle
+            )
+        )
+            
         super.init(nibName: nil, bundle: nil)
         
         self.modalPresentationStyle = .custom
         self.transitioningDelegate = self.transition
         
-        backgroundView.translatesAutoresizingMaskIntoConstraints = false
-        backgroundView.layer.cornerRadius = kCornerRadius
+        backgroundView.layer.cornerRadius = cornerRadius
         backgroundView.layer.masksToBounds = true
         
-        activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
-        activityIndicatorView.hidesWhenStopped = true
+        if let color = activityIndicatorColor ?? LoadingAlertController.defaultActivityIndicatorColor {
+            activityIndicatorView.color = color
+        }
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
         
-        activityIndicatorView.color = activityIndicatorColor ?? LoadingAlertController.defaultActivityIndicatorColor
-        
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(backgroundView)
+        
+        activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(activityIndicatorView)
         
         NSLayoutConstraint.activate([
             
-            backgroundView.widthAnchor.constraint(equalToConstant: kSpan),
-            backgroundView.widthAnchor.constraint(equalTo: backgroundView.heightAnchor),
+            backgroundView.widthAnchor.constraint(equalToConstant: size.width),
+            backgroundView.heightAnchor.constraint(equalToConstant: size.height),
             backgroundView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             backgroundView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             
             activityIndicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             activityIndicatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     public override func viewWillAppear(_ animated: Bool) {
