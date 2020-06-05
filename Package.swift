e852ef7412d909ec8e344a5328ab7ac8646e8095
Brandon Erbschloe <berbschloe@gmail.com>
2020-06-05 11:54:40 -0500

    Cleanup Package.swift

--- Package.swift
+++ Package.swift
@@ -3,26 +3,15 @@
 import PackageDescription
 
 let package = Package(
-  name: "LoadingAlert",
-  platforms: [
-    .iOS(.v11)
-  ],
-  products: [
-    .library(
-      name: "LoadingAlert", 
-      targets: ["LoadingAlert"]
-    ),
-  ],
-  targets: [
-    .target(
-      name: "LoadingAlert", 
-      path: "LoadingAlert"
-    ),
-    .testTarget(
-      name: "LoadingAlertTests", 
-      dependencies: ["LoadingAlert"], 
-      path: "LoadingAlertTests"
-    ),
-  ],
-  swiftLanguageVersions: [.v5]
+    name: "LoadingAlert",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "LoadingAlert", targets: ["LoadingAlert"]),
+    ],
+    targets: [
+        .target(name: "LoadingAlert", path: "LoadingAlert"),
+        .testTarget(name: "LoadingAlertTests", dependencies: ["LoadingAlert"]),
+    ]
 )
